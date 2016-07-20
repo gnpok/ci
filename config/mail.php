f1cdeb925e92da81812bc017bb044ea1c2280129
gnp <742926717@qq.com>
2016-07-20 14:41:17 +0800

    添加邮件配置

---
+++ config/mail.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');		
+$config = array(
+	'mail' => array(
+		'protocol' 	=> 'smtp',  
+	    'smtp_host' => 'smtp.163.com',  
+	    'smtp_user' => 'minitest01@163.com',  
+	    'smtp_pass' => 'wo12417',  
+	    'smtp_port' => '25',  
+	    'charset' 	=> 'utf-8',  
+	    'wordwrap' 	=> TRUE,  
+	    'mailtype' 	=> 'html',
+	)
+);
+
